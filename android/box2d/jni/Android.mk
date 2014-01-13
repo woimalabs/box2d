b707ad14a73bfe529799c50454a6a322f251cbe8
Antti Peuhkurinen <antti.peuhkurinen@woimasolutions.com>
2014-01-13 23:32:59 +0200

    Android file listing OK from parent project

--- android/box2d/jni/Android.mk
+++ android/box2d/jni/Android.mk
@@ -1,18 +1,26 @@
 LOCAL_PATH:= $(call my-dir)
-
-LS_CPP=$(subst $(1)/,,$(wildcard $(1)/$(2)/*.cpp))
-BOX2D_CPP:= $(call LS_CPP,$(LOCAL_PATH), ../../../box2d-2.2.1/Box2D/Collision) \
-            $(call LS_CPP,$(LOCAL_PATH), ../../../box2d-2.2.1/Box2D/Collision/Shapes) \
-            $(call LS_CPP,$(LOCAL_PATH), ../../../box2d-2.2.1/Box2D/Common) \
-            $(call LS_CPP,$(LOCAL_PATH), ../../../box2d-2.2.1/Box2D/Dynamics) \
-            $(call LS_CPP,$(LOCAL_PATH), ../../../box2d-2.2.1/Box2D/Dynamics/Contacts) \
-            $(call LS_CPP,$(LOCAL_PATH), ../../../box2d-2.2.1/Box2D/Dynamics/Joints) \
-            $(call LS_CPP,$(LOCAL_PATH), ../../../box2d-2.2.1/Box2D/Rope)
-
 include $(CLEAR_VARS)
-
 LOCAL_MODULE:= box2d
-LOCAL_SRC_FILES:= $(BOX2D_CPP)
+
+# files
+SOURCE:= ../../../box2d-2.2.1/Box2D
+FILES_COLLISION         := $(wildcard $(LOCAL_PATH)/$(SOURCE)/Collision/*.cpp)
+FILES_COLLISION_SHAPES  := $(wildcard $(LOCAL_PATH)/$(SOURCE)/Collision/Shapes/*.cpp)
+FILES_COMMON            := $(wildcard $(LOCAL_PATH)/$(SOURCE)/Common/*.cpp)
+FILES_DYNAMICS          := $(wildcard $(LOCAL_PATH)/$(SOURCE)/Dynamics/*.cpp)
+FILES_DYNAMICS_CONTACTS := $(wildcard $(LOCAL_PATH)/$(SOURCE)/Dynamics/Contacts/*.cpp)
+FILES_DYNAMICS_JOINTS   := $(wildcard $(LOCAL_PATH)/$(SOURCE)/Dynamics/Joints/*.cpp)
+FILES_ROPE              := $(wildcard $(LOCAL_PATH)/$(SOURCE)/Rope/*.cpp)
+
+# add files to sources
+LOCAL_SRC_FILES += $(FILES_COLLISION:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES += $(FILES_COLLISION_SHAPES:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES += $(FILES_COMMON:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES += $(FILES_DYNAMICS:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES += $(FILES_DYNAMICS_CONTACTS:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES += $(FILES_DYNAMICS_JOINTS:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES += $(FILES_ROPE:$(LOCAL_PATH)/%=%)
+
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../box2d-2.2.1
 LOCAL_C_INCLUDES := $(LOCAL_EXPORT_C_INCLUDES)
 
