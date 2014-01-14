4b8a724a08cbf770cfba56a78f9903fe07e979d2
Antti Peuhkurinen <antti.peuhkurinen@woimasolutions.com>
2014-01-14 19:36:01 +0200

    ios build OK

---
+++ ios/box2d/box2dTests/box2dTests.m
@@ -0,0 +1,34 @@
+//
+//  box2dTests.m
+//  box2dTests
+//
+//  Created by Antti Peuhkurinen on 14.1.2014.
+//  Copyright (c) 2014 Woima Solutions Oy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface box2dTests : XCTestCase
+
+@end
+
+@implementation box2dTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
